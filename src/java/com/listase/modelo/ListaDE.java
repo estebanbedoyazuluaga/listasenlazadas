b896a079df128bae2f9f7498312a93117791c7f7
ESTEBAN <ESTEBAN@Laptop>
2020-04-11 05:22:49 -0500

    agregado un bean de sesion para manejar la página de listas doblemente enlazadas

--- src/java/com/listase/modelo/ListaDE.java
+++ src/java/com/listase/modelo/ListaDE.java
@@ -1,20 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.listase.modelo;
 
-/**
- *
- * @author ESTEBAN
- */
+import com.listase.excepciones.InfanteExcepcion;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaDE {
     private NodoDE cabeza;
     
     public ListaDE(NodoDE cabeza){
         this.cabeza=cabeza;
     }
+    public ListaDE(){}
+    
+    public List obtenerListaInfantes() {
+        List<Infante> listado = new ArrayList<>();
+        //Un método recursivo que recoora mis infantes y que sacando la
+        // info la adicione een el string
+        listarInfantes(listado);
+        return listado;
+    }
+    public void listarInfantes(List listado) {
+        if (cabeza != null) {
+            NodoDE temp = cabeza;
+            while (temp != null) {
+                //listado += temp.getDato()+"\n";
+                listado.add(temp.getDato());
+                temp = temp.getSiguiente();
+            }
+        }
+
+    }
+    
     
     public void adicionarNodo(NodoDE nuevo){
         if (cabeza==null)
@@ -57,24 +73,80 @@ public class ListaDE {
         }
     }
     
-    public void eliminarNodo(NodoDE delete){
+    public void eliminarNodo(NodoDE delete) throws InfanteExcepcion {
         if(cabeza!=null) {
             NodoDE temp=cabeza;
-            while (temp!=delete){
+            while (temp!=delete && temp.getSiguiente() != null){
+                temp=temp.getSiguiente();
+            }
+            if (temp==delete) {
+                temp.getAnterior().setSiguiente(temp.getSiguiente());
+            } else {
+                throw new InfanteExcepcion("El código no se encuentra en la lista.");
+            }
+        }
+    }
+    public void eliminarNodo(Infante delete) throws InfanteExcepcion {
+        if(cabeza!=null) {
+            NodoDE temp=cabeza;
+            while (temp.getDato()!=delete && temp.getSiguiente() != null){
                 temp=temp.getSiguiente();
             }
             temp.getAnterior().setSiguiente(temp.getSiguiente());
-        }
-    }
-    public void eliminarNodo(Infante delete){
-        if(cabeza!=null) {
-            NodoDE temp=cabeza;
-            while (temp.getDato()!=delete){
-                temp=temp.getSiguiente();
+            if (temp.getDato()==delete) {
+                temp.getAnterior().setSiguiente(temp.getSiguiente());
+            } else {
+                throw new InfanteExcepcion("El código no se encuentra en la lista.");
             }
-            temp.getAnterior().setSiguiente(temp.getSiguiente());
         }
     }
+    public short contarNodos() {
+        if (cabeza == null) {
+            return 0;
+        } else {
+            //llamar a mi ayudante
+            NodoDE temp = cabeza;
+            short cont = 1;
+            while (temp.getSiguiente() != null) {
+                temp = temp.getSiguiente();
+                cont++;
+            }
+            return cont;
+        }
+    }
+    public void invertirLista() {
+        if (cabeza != null) {
+            //Crear una lista temporal la cabeza de la lista temporal está vacía
+            ListaDE listaTemporal = new ListaDE();
+            // Llamo un ayudante
+            NodoDE temp = cabeza;
+            //Recorro la lista de principio a fin
+            while (temp != null) {
+                //Parado en cada nodo , se extrae la información y se
+                // envía a la otra lista al inicio
+                listaTemporal.adicionarNodoAlInicio(temp.getDato());
+                temp = temp.getSiguiente();
+            }
+            //Igualo la cabeza de mi lista principal a la cabeza de la lista temporal
+            cabeza = listaTemporal.getCabeza();
+        }
+    }
+    public void eliminarInfante(short delete) throws InfanteExcepcion {
+        if (cabeza != null) {
+            NodoDE temp = cabeza;
+            while (temp.getDato().getCodigo() != delete && temp.getSiguiente() != null) {
+                temp = temp.getSiguiente();
+            }
+            if (temp.getDato().getCodigo() == delete) {
+                temp.getAnterior().setSiguiente(temp.getSiguiente());
+            } else {
+                throw new InfanteExcepcion("El código no se encuentra en la lista.");
+            }
+
+        }
+        throw new InfanteExcepcion("La lista de infantes está vacía");
+    }
+    
     
     //<editor-fold defaultstate="collapsed" desc="Métodos de acceso">
     public NodoDE getCabeza() {
@@ -84,6 +156,6 @@ public class ListaDE {
     public void setCabeza(NodoDE cabeza) {
         this.cabeza = cabeza;
     }
-//</editor-fold>
+    //</editor-fold>
     
 }
